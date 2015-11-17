b3efd1484e5225c0a6d5c2672e73b20a7928edf4
simonetma <simone.battiste-allyene@tma1.com>
2015-11-17 14:30:51 -0500

    wip route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :graduates
   
   root 'graduates#index'
+  
+  mount API::Base, at: "/"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
