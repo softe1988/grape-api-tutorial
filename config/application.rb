6b7efbf12092b8e8cfe3868a771f858ed70306b9
simonetma <simone.battiste-allyene@tma1.com>
2015-11-17 14:34:59 -0500

    wip app.rb

--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,15 @@ module GrapeApp
     # config.i18n.default_locale = :de
   end
 end
+
+module API  
+  class Application < Rails::Application
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, 
+            :post, :put, :delete, :options]
+      end
+    end
+    config.active_record.raise_in_transactional_callbacks = true
+  end
