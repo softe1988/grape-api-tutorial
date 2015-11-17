240b05c79781e30788bc1a581c92dcf7057ac10c
simonetma <simone.battiste-allyene@tma1.com>
2015-11-17 14:39:43 -0500

    wip

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,8 +1,18 @@
- module API  
+require "grape-swagger"
+
+module API  
   module V1
     class Base < Grape::API
       mount API::V1::Graduates
       # mount API::V1::AnotherResource
+
+      add_swagger_documentation(
+        api_version: "v1",
+        hide_documentation_path: true,
+        mount_path: "/api/v1/swagger_doc",
+        hide_format: true
+      )
     end
   end
 end  
+  
